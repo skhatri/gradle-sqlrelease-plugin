c6631ddc229e0e96b6cd3729e6a42b3a3f119dff
Suresh Khatri <servlet2@msn.com>
2013-11-28 00:38:22 +1100

    added Oracle query support

--- src/main/groovy/com/github/skhatri/QueryProvider.groovy
+++ src/main/groovy/com/github/skhatri/QueryProvider.groovy
@@ -9,7 +9,11 @@ enum QueryProvider {
             '        insert into version_info(id, version, filename, app, starttime) values(@maxid, ?, ?, ?, getdate())\n' +
             '        END\n', 'update version_info set endtime = getdate() where app = ? and version = ? and id = (select max(id) from version_info where version=?)'),
     MYSQL('insert into version_info(version, filename, app, starttime) values(?, ?, ?, now())',
-            'update version_info v1 set v1.endtime = now() where v1.id in (select id from ( select max(id) as id from version_info where app = ? and version = ? ) as tmp_table)')
+            'update version_info v1 set v1.endtime = now() where v1.id in (select id from ( select max(id) as id from version_info where app = ? and version = ? ) as tmp_table)'),
+    ORACLE('insert into version_info(id, version, filename, app, starttime) values((select coalesce(max(id), 0) + 1), ?, ?, ?, systimestamp)',
+            'update version_info set endtime = systimestamp where app = ? and version = ? and id = (select max(id) from version_info where version=?)')
+
+
 
     private String start
     private String update
